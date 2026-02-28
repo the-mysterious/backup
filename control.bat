b24f42ec28605c2b4f1161c22ff5177c3b257a65
Mysterious <74603575+the-mysterious@users.noreply.github.com>
2026-02-28 10:28:50 +0100

    Modif

--- control.bat
+++ control.bat
@@ -63,29 +63,6 @@ if "%tools%"=="3" start explorer
 pause
 goto menu
 
-:MAJ
-cls
-echo Execution en tant que administrateur...
-
-:: Vérifier s'il s'agit d'une élévation avec privilèges déjà enregistrés
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-:: Si l'erreur de cacls est 0 (pas d'erreur), alors nous avons déjà les privilèges administratifs.
-if '%errorlevel%' == '0' goto :continue
-
-:: Sinon, exécuter en tant qu'administrateur
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs"
-exit /B
-
-:continue
-echo Execution en tant que administrateur reussie.
-
-winget upgrade --all
-pause
-goto menu
-
 :Quitter
 echo Merci d'avoir utiliser le Miku controle.
 pause exit
