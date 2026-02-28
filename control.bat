9dac2b8d9560a7c6788974b753a61228cf712779
Mysterious <74603575+the-mysterious@users.noreply.github.com>
2026-02-28 10:22:35 +0100

    Modif erreur

--- control.bat
+++ control.bat
@@ -5,7 +5,7 @@ color
 :menu 
 cls
 echo ===============================
-echo         Cestre de controle
+echo         Centre de controle
 echo ===============================
 echo.
 echo 1 - Nettoyage disque
@@ -13,6 +13,7 @@ echo 2 - Verification system
 echo 3 - Infos PC
 echo 4 - Outils Windows
 echo 5 - Quitter
+echo 6 - Mise à jour
 echo.
 
 set /p choix="choix :"
@@ -21,6 +22,7 @@ if "%choix%"=="2" goto verification
 if "%choix%"=="3" goto Infos
 if "%choix%"=="4" goto Outils
 if "%choix%"=="5" goto Quitter
+if "%choix%"=="6" goto MAJ
 echo Choix invalide. appuier sur une touche pour recommencer.
 pause >nul
 goto menu
@@ -58,6 +60,29 @@ set /p tools="choisissez un outil : "
 if "%tools%"=="1" start taskmgr
 if "%tools%"=="2" start cmd
 if "%tools%"=="3" start explorer
+pause
+goto menu
+
+:MAJ
+cls
+echo Execution en tant que administrateur...
+
+:: Vérifier s'il s'agit d'une élévation avec privilèges déjà enregistrés
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+:: Si l'erreur de cacls est 0 (pas d'erreur), alors nous avons déjà les privilèges administratifs.
+if '%errorlevel%' == '0' goto :continue
+
+:: Sinon, exécuter en tant qu'administrateur
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+
+:continue
+echo Execution en tant que administrateur reussie.
+
+winget upgrade --all
 pause
 goto menu
 
