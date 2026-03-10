97fc11170cad0f566ff54817ec81a2784f7f0bde
Mysterious <74603575+the-mysterious@users.noreply.github.com>
2026-03-10 22:54:00 +0100

    Verif si admin

--- control.bat
+++ control.bat
@@ -1,4 +1,21 @@
 @echo off
+echo Execution en tant que administrateur...
+
+:: Vérifier s'il s'agit d'une élévation avec privilèges déjà enregistrés
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+:: Si l'erreur de cacls est 0 (pas d'erreur), alors nous avons déjà les privilèges administratifs.
+if '%errorlevel%' == '0' goto :continue
+
+:: Sinon, exécuter en tant qu'administrateur
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+
+:continue
+echo Execution en tant que administrateur reussie.
+
 title Centre de controle PC 
 color 
 
